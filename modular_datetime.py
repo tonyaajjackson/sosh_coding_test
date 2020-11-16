89c3b758d29008e4337acbe6104e5d15c7ca0ccd
Tony Jackson <tony.aaj.jackson@gmail.com>
2020-11-15 20:59:56 -0800

    Change name of ModularDatetime, add DatetimeModDay
    ModularDatetime doesn't tell you its modulus, so the name is unclear.
    Changed ModularDatetime to DatetimeModWeek to make name clear.
    Added DatetimeModDay and its tests to make handling am/pm easier.

--- modular_datetime.py
+++ modular_datetime.py
@@ -2,7 +2,7 @@ from mod import Mod
 from datetime import datetime
 
 
-class ModularDatetime:
+class DatetimeModWeek:
     def __new__(self, day, hour, minute):
         assert (day >= 0 and day < 7), "Day must be between 0 and 6"
         assert (hour >= 0 and hour < 24), "Hour must be between 0 and 23"
@@ -14,7 +14,7 @@ class ModularDatetime:
         return Mod(raw_datetime, seconds_per_week)
 
 
-def test_modular_datetime():
+def test_datetime_mod_week():
     # Tests that should fail
     invalid_inputs = [
         [-1, 10, 10, "Day"],
@@ -27,31 +27,80 @@ def test_modular_datetime():
 
     for [d, h, m, msg] in invalid_inputs:
         try:
-            ModularDatetime(d, h, m)
+            DatetimeModWeek(d, h, m)
         except AssertionError as e:
             assert msg in e.args[0]
 
     # Tests that should pass
-    one_day = ModularDatetime(1, 0, 0)
-    one_hour = ModularDatetime(0, 1, 0)
+    one_day = DatetimeModWeek(1, 0, 0)
+    one_hour = DatetimeModWeek(0, 1, 0)
 
     # Addition without overflow
-    monday_9_04 = ModularDatetime(0, 9, 4)
+    monday_9_04 = DatetimeModWeek(0, 9, 4)
     no_overflow_result = monday_9_04 + one_hour
-    assert no_overflow_result == ModularDatetime(0, 10, 4)
+    assert no_overflow_result == DatetimeModWeek(0, 10, 4)
 
     # Subtraction without underflow
     no_underflow_result = monday_9_04 - one_hour
-    assert no_underflow_result == ModularDatetime(0, 8, 4)
+    assert no_underflow_result == DatetimeModWeek(0, 8, 4)
 
     # Addition with overflow
-    sunday_23_11 = ModularDatetime(6, 23, 11)
+    sunday_23_11 = DatetimeModWeek(6, 23, 11)
     overflow_result = sunday_23_11 + one_day
-    assert overflow_result == ModularDatetime(0, 23, 11)
+    assert overflow_result == DatetimeModWeek(0, 23, 11)
 
     # Subtraction with underflow
     underflow_result = monday_9_04 - one_day
-    assert underflow_result == ModularDatetime(6, 9, 4)
+    assert underflow_result == DatetimeModWeek(6, 9, 4)
+
+
+class DatetimeModDay:
+    def __new__(self, hour, minute):
+        assert (hour >= 0 and hour < 24), "Hour must be between 0 and 23"
+        assert (minute >= 0 and minute < 60), "Minute must be between 0 and 59"
+
+        # Value is in seconds for easy interchange with Unix Epoch format
+        seconds_per_day = 24*60*60
+        raw_datetime = hour*60*60 + minute*60
+        return Mod(raw_datetime, seconds_per_day)
+
+
+def test_datetime_mod_day():
+    # Tests that should fail
+    invalid_inputs = [
+        [-1, 23, "Hour"],
+        [24, 56, "Hour"],
+        [13, -1, "Minute"],
+        [7, 60, "Minute"]
+    ]
+
+    for [h, m, msg] in invalid_inputs:
+        try:
+            DatetimeModDay(h, m)
+        except AssertionError as e:
+            assert msg in e.args[0]
+
+    # Tests that should pass
+    one_hour = DatetimeModDay(1, 0)
+
+    # Addition without overflow
+    time_9_04 = DatetimeModDay(9, 4)
+    no_overflow_result = time_9_04 + one_hour
+    assert no_overflow_result == DatetimeModDay(10, 4)
+
+    # Subtraction without underflow
+    no_underflow_result = time_9_04 - one_hour
+    assert no_underflow_result == DatetimeModDay(8, 4)
+
+    # Addition with overflow
+    time_23_11 = DatetimeModDay(23, 11)
+    overflow_result = time_23_11 + one_hour
+    assert overflow_result == DatetimeModDay(0, 11)
+
+    # Subtraction with underflow
+    time_0_23 = DatetimeModDay(0, 25)
+    underflow_result = time_0_23 - one_hour
+    assert underflow_result == DatetimeModDay(23, 25)
 
 
 def datetime_in_range(start, end, current):
@@ -68,13 +117,13 @@ def test_current_in_range():
     test_inputs = [
         [
             # No overflow
-            ModularDatetime(1, 0, 0),
-            ModularDatetime(3, 0, 0)
+            DatetimeModWeek(1, 0, 0),
+            DatetimeModWeek(3, 0, 0)
         ],
         [
             # Overflow
-            ModularDatetime(6, 0, 0),
-            ModularDatetime(2, 0, 0)
+            DatetimeModWeek(6, 0, 0),
+            DatetimeModWeek(2, 0, 0)
         ]
     ]
 
@@ -83,7 +132,7 @@ def test_current_in_range():
         assert datetime_in_range(
             start,
             end,
-            start - ModularDatetime(1, 0, 0)
+            start - DatetimeModWeek(1, 0, 0)
         ) == False
 
         # Start
@@ -97,7 +146,7 @@ def test_current_in_range():
         assert datetime_in_range(
             start,
             end,
-            start + ModularDatetime(1, 0, 0)
+            start + DatetimeModWeek(1, 0, 0)
         ) == True
 
         # End
@@ -109,5 +158,6 @@ def test_current_in_range():
 
 
 # Tests
-test_modular_datetime()
+test_datetime_mod_week()
+test_datetime_mod_day()
 test_current_in_range()
